f66a2b9f3888b0450c338146baff84a767930bce
Hiru Wijemanne <hiruneshan@gmail.com>
2025-02-09 21:57:26 -0500

    change in the code to prevent the int. overflow

--- Palindrome Number/Solution.cpp
+++ Palindrome Number/Solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
 
         int reversedNum = 0;
-        int originalNum = x;
-
-        while (x != 0) {
+        while (x > reversedNum) {
             reversedNum = reversedNum * 10 + x % 10;
             x /= 10;
         }
 
-        return reversedNum == originalNum;
+        return x == reversedNum || x == reversedNum / 10;
     }
 };
